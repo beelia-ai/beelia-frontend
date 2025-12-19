9b1090457d91cd0015465b2310172a80c01e7778
NAMAN BHATT <naman@decodes.dev>
2025-12-19 16:14:31 +0530

    uppdate new section

--- app/(pages)/home/components/AboutProduct.tsx
+++ app/(pages)/home/components/AboutProduct.tsx
@@ -1,11 +1,23 @@
 'use client'
 
+import Image from 'next/image'
+
 export function AboutProduct() {
   return (
     <div className="relative min-h-screen w-full bg-transparent">
       {/* Section content */}
-      <div className="relative z-10">
-        {/* Blank content - ready for future additions */}
+      <div className="relative z-10 flex flex-col items-center justify-start pt-32">
+        {/* OneStop Image - positioned at top where globe stops */}
+        <div className="w-full flex justify-center mb-12">
+          <Image
+            src="/images/Onestop.png"
+            alt="OneStop"
+            width={800}
+            height={200}
+            className="w-auto h-auto object-contain"
+            priority
+          />
+        </div>
       </div>
     </div>
   )
